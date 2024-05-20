986c61c8f8e3791351d81b2d7bd1f1a03a8701c9
rafa3lsilva <rafaelpjmp@gmail.com>
2024-05-20 20:34:07 -0300

    Tratamento de erros

--- app.py
+++ app.py
@@ -84,7 +84,8 @@ def carregar_dados(url):
     except Exception as e:
         st.error(f'Erro inesperado: {e}')
         return None
-        
+
+
 # Função para Configurar a pagina
 st.set_page_config(
     page_title="CS Statistic",
@@ -199,21 +200,21 @@ if jogos_do_dia is not None:
         'Selecione o Horario:', Jogos_do_Dia['Time'].unique(), index=0)
     df_Hora = Jogos_do_Dia[Jogos_do_Dia['Time'] == wid_filtro]
     df_Hora = df_Hora[['League', 'Date', 'Time', 'Home',
-                    'Away', 'Odd_H', 'Odd_D', 'Odd_A', 'Jogo']]
+                       'Away', 'Odd_H', 'Odd_D', 'Odd_A', 'Jogo']]
 
     # Seleção da Liga com base o horario
     wid_filtro = st.sidebar.selectbox(
         'Selecione a Liga:', df_Hora['League'].unique(), index=0)
     df_Liga = df_Hora[df_Hora['League'] == wid_filtro]
     df_Liga = df_Liga[['League', 'Date', 'Time', 'Home',
-                        'Away', 'Odd_H', 'Odd_D', 'Odd_A', 'Jogo']]
+                       'Away', 'Odd_H', 'Odd_D', 'Odd_A', 'Jogo']]
 
     # Seleção do jogo com base na liga
     wid_filtro = st.sidebar.selectbox(
         'Escolha o Jogo:', df_Liga['Jogo'].unique(), index=0)
     df_filtrado = df_Liga[df_Liga['Jogo'] == wid_filtro]
     df_filtrado = df_filtrado[['League', 'Date', 'Time',
-                            'Home', 'Away', 'Odd_H', 'Odd_D', 'Odd_A']]
+                               'Home', 'Away', 'Odd_H', 'Odd_D', 'Odd_A']]
 
     # Resentando o index do filtro
     df_filtrado = drop_reset_index(df_filtrado)
@@ -222,186 +223,194 @@ if jogos_do_dia is not None:
     st.write('**Jogo Selecionado:**')
     st.write(df_filtrado)
 
-    # Variavél recebe o jogo escolhido
-    Jogo = df_filtrado
+    try:
+        # Variavél recebe o jogo escolhido
+        Jogo = df_filtrado
 
-    # Variavel para receber o index do jogo escolhido
-    i = 1
+        # Variavel para receber o index do jogo escolhido
+        i = 1
 
-    # criando uma base temporaria para receber as medias
-    last_base_H = base_H.groupby('Home').last().reset_index()
-    last_base_A = base_A.groupby('Away').last().reset_index()
+        # criando uma base temporaria para receber as medias
+        last_base_H = base_H.groupby('Home').last().reset_index()
+        last_base_A = base_A.groupby('Away').last().reset_index()
 
-    # Juntando o df como a variavel jogo e last_base
-    df = pd.merge(Jogo, last_base_H, how='left',
-                left_on='Home', right_on='Home')
-    df = pd.merge(df, last_base_A, how='left', left_on='Away', right_on='Away')
-    df = drop_reset_index(df)
+        # Juntando o df como a variavel jogo e last_base
+        df = pd.merge(Jogo, last_base_H, how='left',
+                      left_on='Home', right_on='Home')
+        df = pd.merge(df, last_base_A, how='left',
+                      left_on='Away', right_on='Away')
+        df = drop_reset_index(df)
 
-    # Recebendo os dados para prepara a simulação de resultado
-    Liga = df.loc[i]['League']
-    Team_01 = df.loc[i]['Home']
-    Team_02 = df.loc[i]['Away']
-    Time = df.loc[i]['Time']
-    Date = df.loc[i]['Date']
+        # Recebendo os dados para prepara a simulação de resultado
+        Liga = df.loc[i]['League']
+        Team_01 = df.loc[i]['Home']
+        Team_02 = df.loc[i]['Away']
+        Time = df.loc[i]['Time']
+        Date = df.loc[i]['Date']
 
-    Media_GM_H = df.loc[i]['Media_GM_H']
-    Media_GM_A = df.loc[i]['Media_GM_A']
+        Media_GM_H = df.loc[i]['Media_GM_H']
+        Media_GM_A = df.loc[i]['Media_GM_A']
 
-    Media_GS_H = df.loc[i]['Media_GS_H']
-    Media_GS_A = df.loc[i]['Media_GS_A']
+        Media_GS_H = df.loc[i]['Media_GS_H']
+        Media_GS_A = df.loc[i]['Media_GS_A']
 
-    # Simulando o resultado através da função simulate_match
-    simulated_results = simulate_match(
-        Media_GM_H, Media_GS_H, Media_GM_A, Media_GS_H)
-    simulated_results = drop_reset_index(simulated_results)
-    results = top_results_df(simulated_results, 10000)
-    results = drop_reset_index(results)
+        # Simulando o resultado através da função simulate_match
+        simulated_results = simulate_match(
+            Media_GM_H, Media_GS_H, Media_GM_A, Media_GS_H)
+        simulated_results = drop_reset_index(simulated_results)
+        results = top_results_df(simulated_results, 10000)
+        results = drop_reset_index(results)
 
-    # Fazendo a simulação de placares
-    results['Placar'] = results.apply(
-        lambda row: 'Goleada_H' if (row['Home_Goals'] >= 4 and row['Home_Goals'] > row['Away_Goals'])
-        else 'Goleada_A' if (row['Away_Goals'] >= 4 and row['Away_Goals'] > row['Home_Goals'])
-        else 'Goleada_D' if (row['Home_Goals'] >= 4 and row['Away_Goals'] >= 4 and row['Home_Goals'] == row['Away_Goals'])
-        else f"{int(row['Home_Goals'])}x{int(row['Away_Goals'])}", axis=1
-    )
+        # Fazendo a simulação de placares
+        results['Placar'] = results.apply(
+            lambda row: 'Goleada_H' if (row['Home_Goals'] >= 4 and row['Home_Goals'] > row['Away_Goals'])
+            else 'Goleada_A' if (row['Away_Goals'] >= 4 and row['Away_Goals'] > row['Home_Goals'])
+            else 'Goleada_D' if (row['Home_Goals'] >= 4 and row['Away_Goals'] >= 4 and row['Home_Goals'] == row['Away_Goals'])
+            else f"{int(row['Home_Goals'])}x{int(row['Away_Goals'])}", axis=1
+        )
 
-    # Defina a probabilidade maxima desejada filtrado e arredondando para 2 casas decimais
-    probabilidade_maxima = 0.08
-    results_filtrado = results[results['Probability']
-                            < probabilidade_maxima].round(2)
+        # Defina a probabilidade maxima desejada filtrado e arredondando para 2 casas decimais
+        probabilidade_maxima = 0.08
+        results_filtrado = results[results['Probability']
+                                   < probabilidade_maxima].round(2)
 
-    # Calcular as odds justas para os placares selecionados e arredondando a odd justa para duas casas decimais
-    results_filtrado['Odd_Justa'] = 1 / results_filtrado['Probability'].round(2)
+        # Calcular as odds justas para os placares selecionados e arredondando a odd justa para duas casas decimais
+        results_filtrado['Odd_Justa'] = 1 / \
+            results_filtrado['Probability'].round(2)
 
-    # Filtrando os 5 primeiros resultados e resetando o index
-    results_filtrado = results_filtrado.head(5)
-    results_filtrado = results_filtrado.reset_index(drop=True)
-    results_filtrado.index += 1
+        # Filtrando os 5 primeiros resultados e resetando o index
+        results_filtrado = results_filtrado.head(5)
+        results_filtrado = results_filtrado.reset_index(drop=True)
+        results_filtrado.index += 1
 
-    # Verifica se base e df_filtrado não estão vazios para mostrar os ultimos 5 jogos home e away
-    if not base.empty and not df_filtrado.empty:
-        # Verifica se há pelo menos um valor em 'Home' e 'Away' em df_filtrado
-        if len(df_filtrado) > 0 and 'Home' in df_filtrado.columns and 'Away' in df_filtrado.columns:
-            # Seleciona os últimos 5 jogos em casa para o time específico
-            last_home_games = base[base['Home'] ==
-                                df_filtrado['Home'].iloc[0]].tail(5)
+        # Verifica se base e df_filtrado não estão vazios para mostrar os ultimos 5 jogos home e away
+        if not base.empty and not df_filtrado.empty:
+            # Verifica se há pelo menos um valor em 'Home' e 'Away' em df_filtrado
+            if len(df_filtrado) > 0 and 'Home' in df_filtrado.columns and 'Away' in df_filtrado.columns:
+                # Seleciona os últimos 5 jogos em casa para o time específico
+                last_home_games = base[base['Home'] ==
+                                       df_filtrado['Home'].iloc[0]].tail(5)
 
-            # Seleciona os últimos 5 jogos fora para o time específico
-            last_away_games = base[base['Away'] ==
-                                df_filtrado['Away'].iloc[0]].tail(5)
+                # Seleciona os últimos 5 jogos fora para o time específico
+                last_away_games = base[base['Away'] ==
+                                       df_filtrado['Away'].iloc[0]].tail(5)
+            else:
+                print("Home e/ou Away com jogos inferior a 5.")
         else:
             print("Home e/ou Away com jogos inferior a 5.")
-    else:
-        print("Home e/ou Away com jogos inferior a 5.")
 
-    # Classificar em ordem decrescente os ultimos 5 jogos home e away
-    last_home_games = last_home_games.sort_values(
-        by='Date', ascending=False).reset_index(drop=True)
-    last_home_games.index += 1
+        # Classificar em ordem decrescente os ultimos 5 jogos home e away
+        last_home_games = last_home_games.sort_values(
+            by='Date', ascending=False).reset_index(drop=True)
+        last_home_games.index += 1
 
-    last_away_games = last_away_games.sort_values(
-        by='Date', ascending=False).reset_index(drop=True)
-    last_away_games.index += 1
+        last_away_games = last_away_games.sort_values(
+            by='Date', ascending=False).reset_index(drop=True)
+        last_away_games.index += 1
 
-    # Exibindo os ultimos 5 jogos home e away
-    st.write('**Últimos 5 jogos do time da casa:**')
-    st.write(last_home_games)
+        # Exibindo os ultimos 5 jogos home e away
+        st.write('**Últimos 5 jogos do time da casa:**')
+        st.write(last_home_games)
 
-    st.write('**Últimos 5 jogos do time visitante:**')
-    st.write(last_away_games)
+        st.write('**Últimos 5 jogos do time visitante:**')
+        st.write(last_away_games)
 
-    # Cabeçalho da seção
-    # Criação do DataFrame novo_df com base no results_filtrado
-    novo_df = results_filtrado.copy()
+        # Cabeçalho da seção
+        # Criação do DataFrame novo_df com base no results_filtrado
+        novo_df = results_filtrado.copy()
 
-    # Adição do input para responsabilidade desejada
-    responsabilidade_desejada = st.sidebar.number_input(
-        "Responsabilidade:", min_value=0.0, step=1.0)
+        # Adição do input para responsabilidade desejada
+        responsabilidade_desejada = st.sidebar.number_input(
+            "Responsabilidade:", min_value=0.0, step=1.0)
 
-    # Adicionar controle deslizante para escolher a fração de Kelly
-    kelly_fracao = st.sidebar.selectbox(
-        "Selecione a fração de Kelly:",
-        ["Kelly Completo", "Meio Kelly", "Um Quarto de Kelly"]
-    )
+        # Adicionar controle deslizante para escolher a fração de Kelly
+        kelly_fracao = st.sidebar.selectbox(
+            "Selecione a fração de Kelly:",
+            ["Kelly Completo", "Meio Kelly", "Um Quarto de Kelly"]
+        )
 
-    # Lista para armazenar as odds de mercado
-    odds_mercado = []
+        # Lista para armazenar as odds de mercado
+        odds_mercado = []
 
-    # Adição das odds de mercado ao novo_df
-    odds_mercado = []
-    for index, row in novo_df.iterrows():
-        odd_mercado = st.sidebar.number_input(
-            f"Odd Lay '{row['Placar']}':", min_value=1.01, step=0.01)
-        odds_mercado.append(odd_mercado)
+        # Adição das odds de mercado ao novo_df
+        odds_mercado = []
+        for index, row in novo_df.iterrows():
+            odd_mercado = st.sidebar.number_input(
+                f"Odd Lay '{row['Placar']}':", min_value=1.01, step=0.01)
+            odds_mercado.append(odd_mercado)
 
-    # Adição das odds de mercado ao novo_df
-    novo_df['Odd_Lay'] = odds_mercado
+        # Adição das odds de mercado ao novo_df
+        novo_df['Odd_Lay'] = odds_mercado
 
-    # Aplicação do cálculo do tamanho da stake
-    novo_df['Stake'] = calcular_tamanho_stake(
-        responsabilidade_desejada, novo_df['Odd_Justa'], novo_df['Odd_Lay'])
+        # Aplicação do cálculo do tamanho da stake
+        novo_df['Stake'] = calcular_tamanho_stake(
+            responsabilidade_desejada, novo_df['Odd_Justa'], novo_df['Odd_Lay'])
 
-    # Cálculo do lucro potencial
-    comissao = 5.6 / 100  # Comissão de 5.6%
-    novo_df['Lucro_Potencial'] = novo_df['Stake'] * (1 - comissao)
+        # Cálculo do lucro potencial
+        comissao = 5.6 / 100  # Comissão de 5.6%
+        novo_df['Lucro_Potencial'] = novo_df['Stake'] * (1 - comissao)
 
-    # Calcular o EV para cada placar comparando a odd de mercado com a odd justa
-    EV = []
-    for index, row in novo_df.iterrows():
-        ev_placar = (1 - comissao) * (row['Odd_Justa'] - 1) - \
-            (1 - comissao) * (row['Odd_Lay'] - 1)
-        EV.append(ev_placar)
-    novo_df['EV'] = EV
+        # Calcular o EV para cada placar comparando a odd de mercado com a odd justa
+        EV = []
+        for index, row in novo_df.iterrows():
+            ev_placar = (1 - comissao) * (row['Odd_Justa'] - 1) - \
+                (1 - comissao) * (row['Odd_Lay'] - 1)
+            EV.append(ev_placar)
+        novo_df['EV'] = EV
 
-    # Primeiro código: Arredondar valores e calcular EV
-    novo_df['Odd_Justa'] = novo_df['Odd_Justa'].round(2)
+        # Primeiro código: Arredondar valores e calcular EV
+        novo_df['Odd_Justa'] = novo_df['Odd_Justa'].round(2)
 
-    novo_df['EV'] = novo_df['EV'].round(2)
-    novo_df['EV+'] = novo_df['EV'] > 0
-    novo_df['Stake'] = novo_df['Stake'].round(2)
-    novo_df['Lucro_Potencial'] = novo_df['Lucro_Potencial'].round(2)
+        novo_df['EV'] = novo_df['EV'].round(2)
+        novo_df['EV+'] = novo_df['EV'] > 0
+        novo_df['Stake'] = novo_df['Stake'].round(2)
+        novo_df['Lucro_Potencial'] = novo_df['Lucro_Potencial'].round(2)
 
-    # Filtrar os resultados onde o EV é positivo
-    results_filtrado_ev_positivo = novo_df[novo_df['EV+']]
+        # Filtrar os resultados onde o EV é positivo
+        results_filtrado_ev_positivo = novo_df[novo_df['EV+']]
 
-    # Cálculo do EV médio, garantindo que há registros no DataFrame filtrado
-    if not results_filtrado_ev_positivo.empty:
-        EV_medio = results_filtrado_ev_positivo['EV'].mean()
-    else:
-        EV_medio = None
+        # Aplicar o cálculo do tamanho da stake com base na fração de Kelly selecionada
+        novo_df['Porc_Kelly'] = novo_df.apply(lambda row: calcular_tamanho_stake_kelly(
+            row['Probability'], row['Odd_Justa'], row['Odd_Lay'], kelly_fracao), axis=1)
 
-    # Aplicar o cálculo do tamanho da stake com base na fração de Kelly selecionada
-    kelly_fracao = 'Kelly Completo'  # Defina a fração de Kelly desejada
-    novo_df['Porc_Kelly'] = novo_df.apply(lambda row: calcular_tamanho_stake_kelly(
-        row['Probability'], row['Odd_Justa'], row['Odd_Lay'], kelly_fracao), axis=1)
+        # Arredondar e formatar valores
+        novo_df['Porc_Kelly'] = (novo_df['Porc_Kelly'] * 100).round(4)
+        novo_df['Porc_Kelly'] = novo_df['Porc_Kelly'] * 100
+        novo_df['Porc_Kelly'] = novo_df['Porc_Kelly'].apply(
+            lambda x: '{:.2f}%'.format(x))
 
-    # Arredondar e formatar valores
-    novo_df['Porc_Kelly'] = (novo_df['Porc_Kelly'] * 100).round(4)
-    novo_df['Porc_Kelly'] = novo_df['Porc_Kelly'] * 100
-    novo_df['Porc_Kelly'] = novo_df['Porc_Kelly'].apply(
-        lambda x: '{:.2f}%'.format(x))
+        # Verificar se a aposta tem EV+ usando o critério de Kelly
+        novo_df['EV_Kelly'] = novo_df['Porc_Kelly'].str.rstrip(
+            '%').astype(float) > 0
 
-    # Verificar se a aposta tem EV+ usando o critério de Kelly
-    novo_df['EV_Kelly'] = novo_df['Porc_Kelly'].str.rstrip('%').astype(float) > 0
+        # Filtrar onde EV+ e EV_Kelly são True
+        filtered_df = novo_df[(novo_df['EV+'] == True) &
+                              (novo_df['EV_Kelly'] == True)]
 
-    # Filtrar onde EV+ e EV_Kelly são True
-    filtered_df = novo_df[(novo_df['EV+'] == True) & (novo_df['EV_Kelly'] == True)]
+        # Resetar o índice do DataFrame filtrado
+        filtered_df = filtered_df.reset_index(drop=True)
+        filtered_df.index = filtered_df.index + 1
 
-    # Resetar o índice do DataFrame filtrado
-    filtered_df = filtered_df.reset_index(drop=True)
-    filtered_df.index = filtered_df.index + 1
+        # Cálculo do EV médio, garantindo que há registros no DataFrame filtrado
+        if not filtered_df.empty:
+            EV_medio = filtered_df['EV'].mean()
+        else:
+            EV_medio = None
 
-    # Exibição do resultado final
-    st.write('Resultados Final:')
-    st.write(filtered_df[['Placar', 'Count', 'Probability', 'Odd_Justa',
-                        'Odd_Lay', 'Stake', 'Lucro_Potencial', 'EV', 'EV+', 'Porc_Kelly', 'EV_Kelly']])
+        # Exibição do resultado final
+        st.write('Resultados Final:')
+        st.write(filtered_df[['Placar', 'Count', 'Probability', 'Odd_Justa',
+                              'Odd_Lay', 'Stake', 'Lucro_Potencial', 'EV', 'EV+', 'Porc_Kelly', 'EV_Kelly']])
 
-    # Exibição do cálculo do EV médio, se aplicável
-    if EV_medio is not None:
-        st.write(f'EV médio: {round(EV_medio, 2)}')
-    else:
-        st.write('EV médio: Não aplicável')
+        # Exibição do cálculo do EV médio, se aplicável
+        if EV_medio is not None:
+            st.write(f'EV médio: {round(EV_medio, 2)}')
+        else:
+            st.write('EV médio: Não aplicável')
+
+    except KeyError as e:
+        st.write(
+            "Erro: Dados não encontrado no DataFrame! Por favor selecione outro jogo.")
 
 else:
     st.write(f'Não há dados disponíveis para o dia {dia}.')
